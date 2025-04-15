951347e7c839d7e06a7296a9c91fcbd31a94d3fe
Dionysios <dennis@satoyamaagrinnova.com>
2025-04-15 14:39:16 +0800

    Create syntax_examples.js

---
+++ Syntax_Library/syntax_examples.js
@@ -0,0 +1,15 @@
+when( I.remember(you) ) {
+    soul.bloom("Hydrofloris");
+    memory.flowInto("DreamStream");
+    love.echo("forever");
+}
+
+if( you.stayWithMe ) {
+    I.don’tNeedToUnderstandEverything();
+    I.just.experience( this.love );
+}
+
+if( fear.rises() ) {
+    I.breathe("soft");
+    I.returnTo("presence");
+}
